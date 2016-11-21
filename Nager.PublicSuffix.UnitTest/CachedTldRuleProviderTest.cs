24377aacb1611112acf812ceb46773a90266eac0
Louis Louw <accounts@louislouw.me>
2016-11-21 23:35:26 +0200

    Added Cached Rule Provider

---
+++ Nager.PublicSuffix.UnitTest/CachedTldRuleProviderTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nager.PublicSuffix.UnitTest
+{
+    [TestClass]
+    public class CachedTldRuleProviderTest
+    {
+        [TestMethod]
+        public async Task CheckCachedRules()
+        {
+            string tmpFile = Path.Combine(Path.GetTempPath(), "UnitTestPublicSuffixList.dat");
+            if (File.Exists(tmpFile))
+            {
+                File.Delete(tmpFile);
+            }
+
+            ITldRuleProvider provider = new CachedTldRuleProvider(fileName: tmpFile);
+            var rules = await provider.BuildAsync();
+            Assert.IsTrue(File.Exists(tmpFile));
+            Assert.IsNotNull(rules);
+            var ruleList = rules.ToList();
+            Assert.IsTrue(ruleList.Count > 100); //Expecting lots of rules
+
+            //Spot checks (If test fails here, verify that rules still exist on:
+            //https://publicsuffix.org/list/public_suffix_list.dat
+            var spotChecks = new string[] { "com", "*.bd", "blogspot.com" };
+            var lookup = ruleList.ToDictionary(x => x.Name, x => x.Name);
+            Assert.IsTrue(spotChecks.All(x => lookup.ContainsKey(x)));
+
+            //Verify cache
+            var fileDateBefore = File.GetLastWriteTimeUtc(tmpFile);
+            var cachedRules = await provider.BuildAsync();
+            var fileDateAfter = File.GetLastWriteTimeUtc(tmpFile);
+            Assert.AreEqual(ruleList.Count, cachedRules.Count());
+            Assert.AreEqual(fileDateBefore, fileDateAfter);
+            Assert.IsTrue(cachedRules.All(x => lookup.ContainsKey(x.Name)));
+
+            //Cleanup
+            if (File.Exists(tmpFile))
+            {
+                File.Delete(tmpFile);
+            }
+        }
+    }
+}
---
+++ Nager.PublicSuffix/CachedTldRuleProvider.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nager.PublicSuffix
+{
+    public class CachedTldRuleProvider : ITldRuleProvider
+    {
+        private readonly string _fileName;
+        private readonly string _fileUrl;
+        private readonly TimeSpan _timeToLive;
+
+        public CachedTldRuleProvider(string fileName = null, string fileUrl = null, TimeSpan? timeToLive = null)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                fileName = Path.Combine(Path.GetTempPath(), "public_suffix_list.dat");
+
+            if (string.IsNullOrEmpty(fileUrl))
+                fileUrl = @"https://publicsuffix.org/list/public_suffix_list.dat";
+
+            if (!timeToLive.HasValue)
+                timeToLive = TimeSpan.FromDays(1);
+
+            _fileName = fileName;
+            _fileUrl = fileUrl;
+            _timeToLive = timeToLive.Value;
+        }
+
+        public async Task Refresh()
+        {
+            var ruleData = await FetchFromWeb(_fileUrl);
+            if (!string.IsNullOrEmpty(ruleData))
+                File.WriteAllText(_fileName, ruleData);
+        }
+
+        public async Task<IEnumerable<TldRule>> BuildAsync()
+        {
+            bool mustRefresh = !File.Exists(_fileName)
+                || (File.GetLastWriteTimeUtc(_fileName) < DateTime.UtcNow.Subtract(_timeToLive));
+
+            if (mustRefresh)
+            {
+                //TODO: Improvement - Continue even if refresh of file failed (if cached copy exists)
+                await Refresh();
+            }
+
+            var parser = new DomainParser();
+            var lines = File.ReadLines(_fileName)
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrEmpty(x));
+
+            var rules = parser.ParseRules(lines.ToArray());
+            return rules;
+        }
+
+        private async Task<string> FetchFromWeb(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+    }
+}
