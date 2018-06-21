33e16948dad628a103ddb00d1c2c4d9ad9a7d43b
Phil <phil.dobson@fasthosts.com>
2018-06-21 11:31:29 +0100

    Added an alternative normalizer using IdnMapping

---
+++ Nager.PublicSuffix/IdnMappingNormalizer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Nager.PublicSuffix
+{
+    public class IdnMappingNormalizer : IDomainNormalizer
+    {
+        private readonly IdnMapping _idnMapping = new IdnMapping();
+
+        public List<string> PartlyNormalizeDomainAndExtractFullyNormalizedParts(string domain, out string partlyNormalizedDomain)
+        {
+            partlyNormalizedDomain = null;
+
+            if (string.IsNullOrEmpty(domain))
+            {
+                return null;
+            }
+
+            partlyNormalizedDomain = domain.ToLowerInvariant();
+
+            string punycodeConvertedDomain = partlyNormalizedDomain;
+            if (partlyNormalizedDomain.Contains("xn--"))
+            {
+                punycodeConvertedDomain = this._idnMapping.GetUnicode(partlyNormalizedDomain);
+            }
+
+            return punycodeConvertedDomain
+                .Split('.')
+                .Reverse()
+                .ToList();
+        }
+    }
+}
