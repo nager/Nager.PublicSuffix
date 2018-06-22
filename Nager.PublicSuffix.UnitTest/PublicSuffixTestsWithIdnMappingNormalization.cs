e70567a4883e47e6f49606819c0e058824310a9d
Phil <phil.dobson@fasthosts.com>
2018-06-22 12:18:42 +0100

    Separated out and extended tests dealing with normalization differences

--- Nager.PublicSuffix.UnitTest/PublicSuffixTestsWithIdnMappingNormalization.cs
+++ Nager.PublicSuffix.UnitTest/PublicSuffixTestsWithIdnMappingNormalization.cs
@@ -11,17 +11,5 @@ namespace Nager.PublicSuffix.UnitTest
             var domainParser = new DomainParser(new FileTldRuleProvider("effective_tld_names.dat"), new IdnMappingNormalizer());
             this._domainParser = domainParser;
         }
-
-        [TestMethod]
-        public void UnderscoreCheck()
-        {
-            this.CheckPublicSuffix("_abc.def.ghi.jkl.com", "jkl.com");
-            this.CheckPublicSuffix("abc._def.ghi.jkl.com", "jkl.com");
-            this.CheckPublicSuffix("def._ghi.jkl.com", "jkl.com");
-
-            // These tests demonstrate the wanted behaviour avoiding the 'bug' in Uri construction
-            this.CheckPublicSuffix("abc.def._ghi.jkl.com", "jkl.com");
-            this.CheckPublicSuffix("abc.def.ghi._jkl.com", "_jkl.com");
-        }
     }
 }
