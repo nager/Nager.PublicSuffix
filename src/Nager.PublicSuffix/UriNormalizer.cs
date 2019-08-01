1defe029bf85eef735373a80e7431f54cbe3f5db
Gokay Okutucu <gokay@blinkdot.co>
2019-08-01 21:31:41 +0300

    Fix UriNormalizer checks only for https #30

--- src/Nager.PublicSuffix/UriNormalizer.cs
+++ src/Nager.PublicSuffix/UriNormalizer.cs
@@ -1,38 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Nager.PublicSuffix
-{
-    public class UriNormalizer : IDomainNormalizer
-    {
-        public List<string> PartlyNormalizeDomainAndExtractFullyNormalizedParts(string domain, out string partlyNormalizedDomain)
-        {
+namespace Nager.PublicSuffix {
+    public class UriNormalizer : IDomainNormalizer {
+        static Regex urlChecker = new Regex (@"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+        public List<string> PartlyNormalizeDomainAndExtractFullyNormalizedParts (string domain, out string partlyNormalizedDomain) {
             partlyNormalizedDomain = null;
 
-            if (string.IsNullOrEmpty(domain))
-            {
+            if (string.IsNullOrEmpty (domain)) {
                 return null;
             }
 
             //We use Uri methods to normalize host (So Punycode is converted to UTF-8)
-            if (!domain.Contains("https://"))
-            {
-                domain = string.Concat("https://", domain);
-            }
+            if (!urlChecker.IsMatch (domain))
+                throw new Exception ("This url is broken or invalid");
 
-            if (!Uri.TryCreate(domain, UriKind.RelativeOrAbsolute, out Uri uri))
-            {
+            if (!Uri.TryCreate (domain, UriKind.RelativeOrAbsolute, out Uri uri)) {
                 return null;
             }
 
             partlyNormalizedDomain = uri.Host;
-            var normalizedHost = uri.GetComponents(UriComponents.NormalizedHost, UriFormat.UriEscaped); //Normalize punycode
+            var normalizedHost = uri.GetComponents (UriComponents.NormalizedHost, UriFormat.UriEscaped); //Normalize punycode
 
             return normalizedHost
-                .Split('.')
-                .Reverse()
-                .ToList();
+                .Split ('.')
+                .Reverse ()
+                .ToList ();
         }
     }
 }
