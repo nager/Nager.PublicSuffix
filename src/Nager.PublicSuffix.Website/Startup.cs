dc5c25a935d7b20ff7dad4b388a48a437a0ece1b
Tino Hager <tino.hager@nager.at>
2021-01-30 20:25:11 +0100

    Update website project to .net 5

--- src/Nager.PublicSuffix.Website/Startup.cs
+++ src/Nager.PublicSuffix.Website/Startup.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Nager.PublicSuffix.Extensions;
 
 namespace Nager.PublicSuffix.Website
@@ -28,11 +28,11 @@ namespace Nager.PublicSuffix.Website
                 return structure;
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -44,10 +44,15 @@ namespace Nager.PublicSuffix.Website
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
 
+            app.UseRouting();
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
