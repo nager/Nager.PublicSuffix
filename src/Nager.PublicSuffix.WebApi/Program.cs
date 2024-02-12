478ec9c13366abffb898b5c0403b1161ce299b75
Tino Hager <tino.hager@nager.at>
2024-02-12 22:44:10 +0100

    Update Program.cs

--- src/Nager.PublicSuffix.WebApi/Program.cs
+++ src/Nager.PublicSuffix.WebApi/Program.cs
@@ -21,8 +21,8 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(opt =>
 
 var app = builder.Build();
 
-var ruleProvider = app.Services.GetService<IRuleProvider>();
-await ruleProvider.BuildAsync();
+//var ruleProvider = app.Services.GetService<IRuleProvider>();
+//await ruleProvider.BuildAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
