09f5858503e44f8e62dd8d3c2ec8a1ee9faab29d
Tino Hager <tino.hager@nager.at>
2024-02-12 22:23:44 +0100

    add webapi project

---
+++ src/Nager.PublicSuffix.WebApi/Program.cs
@@ -0,0 +1,42 @@
+using Nager.PublicSuffix;
+using Nager.PublicSuffix.RuleProviders;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IRuleProvider, WebRuleProvider>();
+builder.Services.AddSingleton<IDomainParser, DomainParser>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(opt =>
+{
+    opt.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+var app = builder.Build();
+
+var ruleProvider = app.Services.GetService<IRuleProvider>();
+await ruleProvider.BuildAsync();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/DomainInfo/{domain}", (string domain, IDomainParser domainParser) =>
+{
+    var domainInfo = domainParser.Parse(domain);
+    return domainInfo;
+})
+.WithName("DomainInfo")
+.WithOpenApi();
+
+app.Run();
