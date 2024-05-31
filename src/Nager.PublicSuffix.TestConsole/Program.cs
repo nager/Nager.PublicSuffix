e5609c4755a61c0d8ea628fe50c8868e669e3d3d
Tino Hager <tino.hager@nager.at>
2024-05-31 10:39:16 +0200

    optimize demo project

--- src/Nager.PublicSuffix.TestConsole/Program.cs
+++ src/Nager.PublicSuffix.TestConsole/Program.cs
@@ -1,66 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Nager.PublicSuffix;
 using Nager.PublicSuffix.RuleProviders;
 
-#region CachedHttpRuleProvider
+Console.WriteLine("Run - DemoCachedHttpRuleProviderAsync");
+await DemoCachedHttpRuleProviderAsync();
+Console.WriteLine("Run - DemoSimpleHttpRuleProviderAsync");
+await DemoSimpleHttpRuleProviderAsync();
+Console.WriteLine("Run - DemoLocalFileRuleProviderAsync");
+await DemoLocalFileRuleProviderAsync();
 
-//using var loggerFactory = LoggerFactory.Create(builder =>
-//{
-//    builder.AddConsole();
-//});
-
-//var ruleProviderLogger = loggerFactory.CreateLogger<CachedHttpRuleProvider>();
-
-//IConfiguration configuration = new ConfigurationBuilder()
-//    .AddInMemoryCollection(new List<KeyValuePair<string, string?>>
-//    {
-//        new("Nager:PublicSuffix:DataUrl", "https://publicsuffix.org/list/public_suffix_list1.dat")
-//    })
-//    .Build();
-
-//var httpClient = new HttpClient();
-//var cacheProvider = new Nager.PublicSuffix.RuleProviders.CacheProviders.LocalFileSystemCacheProvider();
-
-//var ruleProvider = new CachedHttpRuleProvider(ruleProviderLogger, configuration, cacheProvider, httpClient);
-
-#endregion
-
-#region SimpleHttpRuleProvider
-
-var ruleProvider = new SimpleHttpRuleProvider();
-
-#endregion
-
-#region LocalFileRuleProvider
-
-//var ruleProvider = new LocalFileRuleProvider("public_suffix_list.dat");
-
-#endregion
-
-await ruleProvider.BuildAsync(ignoreCache: true);
-
-var domainParser = new DomainParser(ruleProvider);
-var domainInfo = domainParser.Parse("www.google.com");
-
-if (domainInfo != null)
+async Task DemoCachedHttpRuleProviderAsync()
 {
-    Console.WriteLine("------------------------------------------------");
-    Console.WriteLine($"{"TLD:", 20} {domainInfo.TopLevelDomain}");
-    Console.WriteLine($"{"FQDN:", 20} { domainInfo.FullyQualifiedDomainName}");
-    Console.WriteLine($"{"RegistrableDomain:", 20} {domainInfo.RegistrableDomain}");
-    Console.WriteLine($"{"Subdomain:",20} {domainInfo.Subdomain}");
-    Console.WriteLine("------------------------------------------------");
+    using var loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    });
+
+    var ruleProviderLogger = loggerFactory.CreateLogger<CachedHttpRuleProvider>();
+
+    IConfiguration configuration = new ConfigurationBuilder()
+        .AddInMemoryCollection(new List<KeyValuePair<string, string?>>
+        {
+            new("Nager:PublicSuffix:DataUrl", "https://publicsuffix.org/list/public_suffix_list.dat")
+        })
+        .Build();
+
+    using var httpClient = new HttpClient();
+    var cacheProvider = new Nager.PublicSuffix.RuleProviders.CacheProviders.LocalFileSystemCacheProvider();
+
+    var ruleProvider = new CachedHttpRuleProvider(ruleProviderLogger, configuration, cacheProvider, httpClient);
+
+    await CheckAsync(ruleProvider);
 }
 
-var validDomains = new[] { "www.google.com", "amazon.com", "microsoft.de", "mail.google.com", "microsoft.de." };
-var invalidDomains = new[] { "www", "uk", "co.uk", ".", "test@test.com" };
-
-foreach (var validDomain in validDomains)
+async Task DemoSimpleHttpRuleProviderAsync()
 {
-    Console.WriteLine($"{validDomain} -> {domainParser.IsValidDomain(validDomain)}");
+    using var ruleProvider = new SimpleHttpRuleProvider();
+
+    await CheckAsync(ruleProvider);
 }
 
-foreach (var invalidDomain in invalidDomains)
+async Task DemoLocalFileRuleProviderAsync()
 {
-    Console.WriteLine($"{invalidDomain} -> {domainParser.IsValidDomain(invalidDomain)}");
+    var ruleProvider = new LocalFileRuleProvider("public_suffix_list.dat");
+
+    await CheckAsync(ruleProvider);
+}
+
+async Task CheckAsync(IRuleProvider ruleProvider)
+{
+    if (!await ruleProvider.BuildAsync(ignoreCache: true))
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Build error");
+        Console.ResetColor();
+        return;
+    }
+
+    var domainParser = new DomainParser(ruleProvider);
+    var domainInfo = domainParser.Parse("www.google.com");
+
+    if (domainInfo != null)
+    {
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine($"{"TLD:",20} {domainInfo.TopLevelDomain}");
+        Console.WriteLine($"{"FQDN:",20} {domainInfo.FullyQualifiedDomainName}");
+        Console.WriteLine($"{"RegistrableDomain:",20} {domainInfo.RegistrableDomain}");
+        Console.WriteLine($"{"Subdomain:",20} {domainInfo.Subdomain}");
+        Console.WriteLine("------------------------------------------------");
+    }
+
+    var validDomains = new[] { "www.google.com", "amazon.com", "microsoft.de", "mail.google.com", "microsoft.de." };
+    var invalidDomains = new[] { "www", "uk", "co.uk", ".", "test@test.com" };
+
+    foreach (var validDomain in validDomains)
+    {
+        Console.WriteLine($"{validDomain} -> {domainParser.IsValidDomain(validDomain)}");
+    }
+
+    foreach (var invalidDomain in invalidDomains)
+    {
+        Console.WriteLine($"{invalidDomain} -> {domainParser.IsValidDomain(invalidDomain)}");
+    }
 }
